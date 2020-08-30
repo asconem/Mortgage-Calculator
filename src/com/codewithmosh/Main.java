9a13006a3c6c690d07b5628cd2a18cd487c05e99
Matthew Ascone <asconem@gmail.com>
2020-08-30 14:03:00 -0400

    Add scanner, principal variable

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,11 +1,17 @@
 package com.codewithmosh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = sc.nextInt();
 
 
     }
