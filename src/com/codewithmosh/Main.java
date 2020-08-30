51b43389ba65675f16f96d63d9c41f863674a934
Matthew Ascone <asconem@gmail.com>
2020-08-30 14:20:02 -0400

    Output monthly mortgage payment, remove extra line when taking input from user

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,5 +1,6 @@
 package com.codewithmosh;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -10,20 +11,23 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Principal: ");
+        System.out.print("Principal: ");
         int principal = sc.nextInt();
 
-        System.out.println("Annual Interest Rate: ");
+        System.out.print("Annual Interest Rate: ");
         double annualInterestRate = sc.nextDouble()/PERCENT;
         double monthlyInterestRate = annualInterestRate/MONTHS_IN_YEAR;
 
-        System.out.println("Period (Years): ");
+        System.out.print("Period (Years): ");
         int years = sc.nextInt();
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double topHalfOfEquation = monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments);
         double bottomHalfOfEquation = Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1;
         double mortgagePayment = principal * (topHalfOfEquation/bottomHalfOfEquation);
+
+        String formattedMortgagePayment = NumberFormat.getCurrencyInstance().format(mortgagePayment);
+        System.out.print("Monthly Mortgage Payment: " + formattedMortgagePayment);
 
     }
 
