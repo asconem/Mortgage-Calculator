a3a355d6e4e8ae93b862f0278010eeff208a31b1
Matthew Ascone <asconem@gmail.com>
2020-08-30 14:00:30 -0400

    Add constants

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -3,6 +3,10 @@ package com.codewithmosh;
 public class Main {
 
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+
 
     }
 
